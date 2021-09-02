2a65b1f9d4930113cb0774eba9412182180cd03a
AlenGeoAlex <71920951+AlenGeoAlex@users.noreply.github.com>
2021-09-02 11:34:59 +0530

    Initial commit

---
+++ src/main/java/me/alen_alex/bwcosmetics/BWCosmetics.java
@@ -0,0 +1,16 @@
+package me.alen_alex.bwcosmetics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BWCosmetics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
