e814a1098227b69dae61c98712568b361c1d4d50
prathamx <66914519+prathamx@users.noreply.github.com>
2021-09-06 20:45:47 +0530

    startdance() method

--- src/main/java/me/alen_alex/bwcosmetics/cosmetics/victorydance/types/Toystick.java
+++ src/main/java/me/alen_alex/bwcosmetics/cosmetics/victorydance/types/Toystick.java
@@ -1,4 +1,21 @@
 package me.alen_alex.bwcosmetics.cosmetics.victorydance.types;
 
-public class Toystick {
+import me.alen_alex.bwcosmetics.cosmetics.victorydance.VictoryDance;
+import me.alen_alex.bwcosmetics.cosmetics.victorydance.VictoryDanceType;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+public class Toystick extends VictoryDance implements Listener {
+    public Toystick(Plugin plugin, Player player){
+        super(plugin,player,player.getLocation(), VictoryDanceType.TOYSTICK);
+    }
+    @Override
+    public void startdance(){
+        if(!hasUsePermission()){
+            return;
+        }
+
+    }
 }
